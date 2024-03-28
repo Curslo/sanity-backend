4eaef0fc2801ff09b7af101a064a5ed5b688369f
Curslo <richardkisivii@gmail.com>
2024-03-28 11:17:04 +0300

    Init

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import {coment} from './coment'
+import {pin} from './pin'
+import {postedBy} from './postedBy'
+import {save} from './save'
+import {user} from './user'
+
+export const schemaTypes = [coment, pin, postedBy, save, user]
